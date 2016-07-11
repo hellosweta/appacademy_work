e38e252b147180dd8c881dcc5896371fc51d0de3
Jeffrey Fiddler <jeff@appacademy.io>
2016-07-10 17:38:53 -0700

    use bundle exec

--- skeleton/lib/assessment01.rb
+++ skeleton/lib/assessment01.rb
@@ -11,7 +11,7 @@ end
 
 # the "calls itself recursively" spec may say that there is no method
 # named "and_call_original" if you are using an older version of
-# rspec. You may ignore this failure.
+# rspec. Make sure you are using 'bundle exec' to run rspec.
 # Also, be aware that the first factorial number is 0!, which is defined
 # to equal 1. So the 2nd factorial is 1!, the 3rd factorial is 2!, etc.
 def factorials_rec(num)
