0ded523c03f7189d363851390e84beae9a2f3147
sewinter <samuelewinter@gmail.com>
2016-07-13 10:41:40 -0700

    edit a01p instructions

--- skeleton/lib/assessment01.rb
+++ skeleton/lib/assessment01.rb
@@ -1,55 +1,53 @@
 require 'byebug'
 class Array
-  # Monkey patch the Array class and add my_inject method. If no accumulator is
-  # passed in then the first element of the array should be used as the accumulator.
+
+  # Monkey patch the Array class and add a my_inject method. If my_inject receives
+  # no argument, then use the first element of the array as the default accumulator.
 
   def my_inject(accumulator = nil)
   end
 end
 
-# `primes(num)` returns an array of the first `num` primes.
-#  You may wish to use the `is_prime?` helper method.
+# primes(num) returns an array of the first "num" primes.
+# You may wish to use an is_prime? helper method.
 
 def is_prime?(num)
 end
 
-def primes(count)
+def primes(num)
 end
 
-# Write a **recursive** implementation of a method that returns the
-# first `n` factorial numbers.
-# Be aware that the first factorial number is 0!, which is defined to
-# equal 1. So the 2nd factorial is 1!, the 3rd factorial is 2!, etc.
+# Write a recursive method that returns the first "num" factorial numbers.
+# Note that the 1st factorial number is 0!, which equals 1. The 2nd factorial
+# is 1!, the 3rd factorial is 2!, etc.
 
 def factorials_rec(num)
 end
 
 class Array
-  # Write a new Array method that will return a hash containing
-  # the locations of all identical elements. The keys are the
-  # duplicated elements, and the values are arrays of their positions,
-  # sorted lowest to highest.
-  # ex:
-  # [1, 3, 4, 3, 0, 3, 0].dups
-  # => { 3 => [1, 3, 5], 0 => [4, 6] }
+
+  # Write an Array#dups method that will return a hash containing the indices of all
+  # duplicate elements. The keys are the duplicate elements; the values are
+  # arrays of their indices in ascending order, e.g.
+  # [1, 3, 4, 3, 0, 3, 0].dups => { 3 => [1, 3, 5], 0 => [4, 6] }
 
   def dups
   end
 end
 
 class String
-  # Write a `String#symmetric_substrings` method that takes a returns
-  # substrings which are equal to their reverse image ("abba" ==
-  # "abba"). We should only include substrings of length > 1.
 
+  # Write a String#symmetric_substrings method that returns an array of substrings
+  # that are palindromes, e.g. "cool".symmetric_substrings => ["oo"]
+  # Only include substrings of length > 1.
 
   def symmetric_substrings
   end
 end
 
 class Array
-  # write a new `Array#merge_sort` method; it should not modify the
-  # array it is called on.
+
+  # Write an Array#merge_sort method; it should not modify the original array.
 
   def merge_sort(&prc)
   end
